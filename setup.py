d173cec3a8ff5576fa4cc6989a1e0d1626980b8c
Joseph Moussa <joeph465@gmail.com>
2021-03-21 23:18:26 -0400

    fix package installer

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import os
-from setuptools import setup, find_namespace_packages
+from setuptools import setup, find_packages
 
 
 def get_requirements():
@@ -8,11 +8,11 @@ def get_requirements():
         return [line.strip() for line in f]
 
 
-print(f"Packages: {find_namespace_packages(include=['alfred.'])}")
+print(f"Packages: {find_packages()}")
 setup(
-    name="alfred-messaging-queue",
+    name="alfred",
     version="0.0.1",
-    packages=find_namespace_packages(include=["alfred."]),
+    packages=find_packages(),
     zip_safe=False,
     install_requires=get_requirements(),
     scripts=[],
