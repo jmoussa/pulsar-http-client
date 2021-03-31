f1a75d49d11e88e74dfcc99b5c9caa18adb5004f
Joseph Moussa <joeph465@gmail.com>
2021-03-30 20:07:28 -0400

    add pulsar topic consuming websocket and add more functionality into PulsarManager

--- alfred/api/pulsar.py
+++ alfred/api/pulsar.py
@@ -1,5 +1,5 @@
 import logging
-from fastapi import Depends, APIRouter
+from fastapi import Depends, APIRouter, WebSocket
 
 # from alfred.requests import PublishRequest, SubscribeRequest, UnsubscribeRequest
 
@@ -14,18 +14,42 @@ from fastapi.responses import JSONResponse
 
 logger = logging.getLogger(__name__)
 router = APIRouter()
-pulsar_manager = PulsarManager()
 
 
 @router.post("/init_subscribe", tags=["Pulsar"])
-async def init_subscribe(
+async def initialize_subscriptions(
     current_user: User = Depends(get_current_active_user), db: MongoClient = Depends(get_nosql_db)
 ):
     """
     Subsribes the current_user to its preconfigured pulsar topics
     """
+    pulsar_manager = PulsarManager(current_user["_id"])
     response = pulsar_manager.init_user_authorized_topics(current_user)
     return JSONResponse(status_code=200, content=response)
+
+
+@router.websocket("/feed/{topic}")
+async def topic_feed(
+    websocket: WebSocket,
+    topic: str,
+    current_user: User = Depends(get_current_active_user),
+):
+    """
+    Websocket that sends messages from a subscribed topic
+    """
+    pulsar_manager = PulsarManager(current_user["_id"])
+    await websocket.accept()
+    try:
+        while True:
+            for response in pulsar_manager.consumer_generator(topic):
+                await websocket.send_text(response)
+    except Exception as ex:
+        template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+        message = template.format(type(ex).__name__, ex.args)
+        logger.error(message)
+        # remove user
+        logger.warning(f"Disconnecting {topic} Websocket")
+        await websocket.close()
 
 
 @router.post("/send_pulsar_message/{topic}", tags=["Pulsar"])
@@ -34,6 +58,7 @@ async def publish_to_pulsar_topic(
     message: PulsarMessage,
     current_user=Depends(get_current_active_user),
 ):
+    pulsar_manager = PulsarManager(current_user["_id"])
     if topic in current_user.authorized_pulsar_topics:
         response = pulsar_manager.publish_message(message.message, topic)
         return JSONResponse(status_code=200, content=response)
--- alfred/controllers/pulsar.py
+++ alfred/controllers/pulsar.py
@@ -8,8 +8,11 @@ logger = logging.getLogger(__name__)
 
 
 class PulsarManager:
-    def __init__(self):
+    def __init__(self, user_id):
+        self.user_id = user_id
         self.conn = client
+        self.consumers = {}
+        self.producers = {}
 
     async def init_user_authorized_topics(self, current_user: User):
         """
@@ -17,20 +20,31 @@ class PulsarManager:
         """
         topics = []
         for topic in current_user.authorized_pulsar_topics:
-            consumer = self.conn.subscribe(topic, "shared")
-            if consumer is not None:
+            self.consumers[topic] = self.conn.subscribe(topic, "shared")
+            if self.consumers[topic] is not None:
                 topics.append(topic)
-
         return {"initialized": topics}
 
     def publish_message(self, message, topic):
-        producer = self.conn.create_producer(topic)
-        producer.send(message.message)
-        return {"topic": topic, "message": message.message, "status": "published"}
+        self.producers[topic] = self.conn.create_producer(topic)
+        if self.producers.get(topic, None):
+            self.producers[topic].send(message.message)
+            return {"topic": topic, "message": message.message, "status": "published"}
+        else:
+            return {"topic": topic, "message": "error creating producer", "status": "not published"}
 
-    async def consumer_generator(consumer):
-        while True:
-            msg = consumer.receive(2000)
-            yield b"Received message '{}' id='{}'".format(msg.data(), msg.message_id())
-            consumer.acknowledge(msg)
-            time.sleep(1)
+    def subscribe(self, topic):
+        self.consumers[topic] = self.conn.subscribe(topic, "shared")
+        self.producers[topic] = self.conn.connect(topic)
+        return self.consumers
+
+    async def consumer_generator(self, topic):
+        if self.consumers.get(topic, None):
+            consumer = self.consumers[topic].subscribe(topic, "shared")
+            while True:
+                msg = consumer.receive(2000)
+                yield b"Received message: '{}' id='{}'".format(msg.data(), msg.message_id())
+                consumer.acknowledge(msg)
+                time.sleep(1)
+        else:
+            raise Exception("No consumer found for topic")
